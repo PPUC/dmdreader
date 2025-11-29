88428e861563f4360740c9bba3aac5cd79d34696
Markus Kalkbrenner <git@kalki.de>
2025-11-29 20:35:23 +0100

    fixed include

--- src/main.c
+++ src/main.c
@@ -2,13 +2,14 @@
 // @see SYS_CLK_MHZ https://github.com/raspberrypi/pico-sdk/releases/tag/2.1.1
 #define SYS_CLK_MHZ 200
 
+#include <stdbool.h>
+
 #include "dmd_reader.h"
+#include "hardware/clocks.h"
 
 int main() {
   // overclock to achieve higher SPI transfer speed
   set_sys_clock_khz(SYS_CLK_MHZ * 1000, true);
-  uint32_t freq = clock_get_hz(clk_sys);
-  printf("System clock: %.2f MHz\n", freq / 1e6);
 
   int result = read_dmd();
 
