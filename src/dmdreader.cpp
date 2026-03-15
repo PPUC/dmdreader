c3a2216a3dda4ded342187180ead5c382524ea66
Jan Vos <jjcvos@hotmail.nl>
2026-03-15 14:15:08 +0100

    document x16 v2 resync

--- src/dmdreader.cpp
+++ src/dmdreader.cpp
@@ -592,13 +592,15 @@ void dmd_dma_reset() {
 void dmd_dma_handler() {
   dmd_set_and_enable_new_dma_target();
 
-  if(dmd_type == DMD_DE_X16_V2) {
-    if(pio_interrupt_get(frame_pio, 5)) {
+  if (dmd_type == DMD_DE_X16_V2) {
+    if (pio_interrupt_get(frame_pio, 5)) {
+      // Due to the complexity of x16 v2, we use this way to re-sync
+      // if the signals are noisy, or whatever else could happen.
       pio_sm_set_enabled(dmd_pio, dmd_sm, false);
       pio_interrupt_clear(frame_pio, 5);
       dmd_dma_reset();
       pio_sm_exec_wait_blocking(dmd_pio, dmd_sm,
-                          pio_encode_mov(pio_y, pio_null));
+                                pio_encode_mov(pio_y, pio_null));
       pio_sm_exec(dmd_pio, dmd_sm, pio_encode_jmp(dmd_offset));
       pio_sm_set_enabled(dmd_pio, dmd_sm, true);
     }
