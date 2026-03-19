b10cc4ce782083352808bdc2b940ce4e61cdabe6
Jan Vos <jjcvos@hotmail.nl>
2026-03-19 09:51:50 +0100

    store in higher bits

--- src/dmdreader.cpp
+++ src/dmdreader.cpp
@@ -612,7 +612,7 @@ void dmd_dma_handler() {
   // Used for Data East 128x16 to correctly align 64x16 + 64x16.
   // Also stores initial data in the hidden part of framebuf.
   int16_t diff_x16 = 0;
-  uint8_t offset_x16 = 255;
+  uint16_t offset_x16 = 511;
 
   // Fix byte order within the buffer
   uint32_t *planebuf = (uint32_t *)currentPlaneBuffer;
@@ -746,7 +746,7 @@ void dmd_dma_handler() {
     // merge the rows and convert from 4bpp to 2bpp with a LUT
     uint32_t *dst, *src1, *src2;
     dst = framebuf + 63; // start in the middle of 128x32 frame
-    src1 = framebuf + 255; // everything is stored from here onwards
+    src1 = framebuf + 511; // everything is stored from here onwards
     src2 = src1 + source_dwordsperline;
 
     if (dmd_type == DMD_DE_X16_V1) {
