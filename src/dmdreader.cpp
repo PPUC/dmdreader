2a9497e345d76a2893f830047bd79092b48362fa
Jan Vos <jjcvos@hotmail.nl>
2026-06-04 00:48:32 +0200

    experimental

--- src/dmdreader.cpp
+++ src/dmdreader.cpp
@@ -629,23 +629,23 @@ void dmd_dma_handler() {
 
   // Fix byte order within the buffer
   uint32_t *planebuf = (uint32_t *)currentPlaneBuffer;
-  buf32_t *v;
-  uint32_t res;
+  //buf32_t *v;
+  //uint32_t res;
   // source_dwordsperframe is not the entire frame buffer if plane history is
   // used. So only the new plane data is fixed here.
-  for (int i = 0; i < source_dwordsperframe; i++) {
-    v = (buf32_t *)planebuf;
-    res = (v->byte3 << 24) | (v->byte2 << 16) | (v->byte1 << 8) | (v->byte0);
-    *planebuf = res;
-    planebuf++;
-  }
+  //for (int i = 0; i < source_dwordsperframe; i++) {
+  //  v = (buf32_t *)planebuf;
+  //  res = (v->byte3 << 24) | (v->byte2 << 16) | (v->byte1 << 8) | (v->byte0);
+  //  *planebuf = res;
+  //  planebuf++;
+  //}
 
   // Get a 32bit pointer to the frame buffer to handle more pixels at once.
   uint32_t *framebuf = (uint32_t *)processingbuf;
 
   bool source_shiftplanesatmerge = (source_mergeplanes == MERGEPLANES_ADDSHIFT);
 
-  planebuf = (uint32_t *)currentPlaneBuffer;
+  //planebuf = (uint32_t *)currentPlaneBuffer;
   // px represents a group of pixels stored in a double word. 8 pixels of 4bit
   // or 16 pixels of 2bit depth.
   for (int px = 0; px < source_dwordsperplane; px++) {
